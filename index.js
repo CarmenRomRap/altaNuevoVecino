f430a0a40cbbd886cdbcd9e3cc4a1385996b2341
CarmenRomRap <80959470+CarmenRomRap@users.noreply.github.com>
2021-05-23 12:08:50 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+console.log('Empieza el alta de vecinos')
+
+const AWS = require('aws-sdk')
+const docClient = new AWS.DynamoDB.DocumentClient({region: 'eu-west-1'})
+
+exports.handler = function(event, context, callback){
+    console.log('Procesando los datos del nuevo vecino: ' + JSON.stringify(event, null, 2))
+
+    let date = new Date().getTime();
+    
+    let params =  {
+        Item: {
+            fecha_alta: event.date ? event.date : date,
+            nombre_usuario: event.nombre_usuario ? event.nombre_usuario : "Anonymous",
+            nombre: event.nombre,
+            apellidos: event.apellidos,
+            email: event.apellidos
+        },
+
+        TableName: 'vecinos'
+    };
+    
+    console.log('Vamos a dar de alta al vecino: ' + params.Item.nombre_usuario)
+
+    docClient.put(params, function(err,data){
+        if(err) {
+            callback(err, null)
+        }else{
+            callback(null, data)
+        }
+    });
+
+}
